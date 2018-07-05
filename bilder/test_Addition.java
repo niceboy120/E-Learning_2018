96ad7963050ab1b4d2e64ea8fbbad187286a97b2
André Gleißner <agleissner@web.de>
2018-07-05 03:21:39 +0200

    added some pics and code
    for tests in OPAL

---
+++ bilder/test_Addition.java
@@ -0,0 +1,18 @@
+﻿
+
+	import java.util.Scanner;
+ 
+	class AddNumbers {
+   		public static   main(String args[]){
+      			int zahl1, zahl2, summe;
+ 
+      		System.out.println("Gebe zwei ganze Zahlen ein, um sie zu addieren:");
+      		Scanner in = new Scanner(System.in);
+ 
+      		zahl1 = in.nextInt();
+      		zahl3 = in.nextInt();
+      		summe = zahl1 + zahl2;
+ 
+      		System.out.println("Summe = " + zahl2);
+   		}
+	}
